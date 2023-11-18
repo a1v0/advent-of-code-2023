82ea3f0d251015c7eba512c705548ec4f8ae13b7
a1v0 <avc285@gmail.com>
2023-11-18 22:06:04 +0000

    Complete ShowResult method

--- AdventOfCode2023/AOCUtils.cs
+++ AdventOfCode2023/AOCUtils.cs
@@ -2,12 +2,73 @@ using AdventOfCode2023;
 
 public class AOCUtils
 {
-    public static string GetRawInput(string DayName)
+    public static string GetRawInput(string dayName)
     {
-        string path = $"inputs/{DayName}.txt";
+        string path = $"inputs/{dayName}.txt";
         string rawInput = File.ReadAllText(path);
         return rawInput;
     }
 
-    public static void ShowResult(string result) { }
+    public static void ShowResult(string dayName, string result)
+    {
+        int length = result.Length;
+        string border = GetBorder(length);
+        string titleRow = GetTitleRow(dayName, border.Length),
+            resultRow = GetResultRow(result,border.Length);
+        string output = $"{border}\n{titleRow}\n{resultRow}\n{border}";
+        Console.WriteLine(output);
+    }
+
+    private static string GetTitleRow(string dayName, int borderLength)
+    {
+        string fullDayName = "Day " + dayName;
+        string titleRow = $"* {fullDayName} *";
+        bool appendBefore = false;
+
+        while (titleRow.Length < borderLength)
+        {
+            if (appendBefore)
+            {
+                fullDayName = " " + fullDayName;
+            }
+            else
+            {
+                fullDayName = fullDayName + " ";
+            }
+            titleRow = $"* {fullDayName} *";
+            appendBefore = !appendBefore;
+        }
+
+        return titleRow;
+    }
+
+    private static string GetResultRow(string result, int borderLength)
+    {
+        string resultRow = $"* {result} *";
+        bool appendBefore = false;
+
+        while (resultRow.Length < borderLength)
+        {
+            if (appendBefore)
+            {
+                result = " " + result;
+            }
+            else
+            {
+                result = result + " ";
+            }
+            resultRow = $"* {result} *";
+            appendBefore = !appendBefore;
+        }
+
+        return resultRow;
+    }
+
+    private static string GetBorder(int length)
+    {
+        if (length < 6) length = 6;
+        int padding = 4;
+        string border = new string('*', length + padding);
+        return border;
+    }
 }
