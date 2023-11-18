2112519e0489f80bf2c34cb404af581a6ced0848
a1v0 <avc285@gmail.com>
2023-11-18 21:29:51 +0000

    Complete GetRawInput method

--- AdventOfCode2023/AOCUtils.cs
+++ AdventOfCode2023/AOCUtils.cs
@@ -4,7 +4,9 @@ public class AOCUtils
 {
     public static string GetRawInput(string DayName)
     {
-        return "";
+        string path = $"inputs/{DayName}.txt";
+        string rawInput = File.ReadAllText(path);
+        return rawInput;
     }
 
     public static void ShowResult(string result) { }
