aae6bc6d51867980f61a969ccde38577cc0619c9
a1v0 <avc285@gmail.com>
2023-11-19 18:36:46 +0000

    Create dummy test for Day 1

---
+++ AdventOfCode2023.Tests/Day01Tests.cs
@@ -0,0 +1,21 @@
+namespace AdventOfCode2023.Tests
+{
+    public class Day01Tests
+    {
+        [Trait("dp", "1,1")]
+        [Fact]
+        public void Test1()
+        {
+            string name = Day1.DayName;
+            Assert.Equal("01", name);
+        }
+
+        [Trait("dp", "1,2")]
+        [Fact]
+        public void Test2()
+        {
+            string name = Day1.DayName;
+            Assert.NotEqual("02", name);
+        }
+    }
+}
