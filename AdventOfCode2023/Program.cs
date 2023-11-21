fba6bfb59fb8671550996351110e9624ca3fd833
a1v0 <avc285@gmail.com>
2023-11-21 22:34:41 +0000

    Create to-do list

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -5,6 +5,41 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine(Day01.DayName);
+        Console.WriteLine(AOCUtils.GetRawInput(Day01.DayName));
+
+        // 
+        // 
+        // accept two args from console (e.g. dotnet run 01 1)
+        // parse inputs as integers and check they're 1-25 and 1 or 2 respectively
+        // switch statement to load correct day's class
+        // add Run method (or similar name) to each day class which takes a parameter of 1 or 2 to define which challenge
+        // 
+        // create parent class for all days
+        // consider setting the day name automatically by doing some string manipulation using something like this.GetType().Name
+        // that will remove much of the boilerplate needed for each day
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
+        // 
     }
 }
 
