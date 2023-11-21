7e31c3850ff9bdc74f647b66eb6abb77a69ad4bf
a1v0 <avc285@gmail.com>
2023-11-21 21:51:40 +0000

    Refactor namespace statement

--- AdventOfCode2023.Tests/Day01Tests.cs
+++ AdventOfCode2023.Tests/Day01Tests.cs
@@ -1,28 +1,27 @@
-namespace AdventOfCode2023.Tests
+namespace AdventOfCode2023.Tests;
+
+public class Day01Tests
 {
-    public class Day01Tests
+    public Day01Tests()
     {
-        public Day01Tests()
-        {
-            Environment.SetEnvironmentVariable("RUN_MODE", "TEST");
-        }
+        Environment.SetEnvironmentVariable("RUN_MODE", "TEST");
+    }
 
-        [Trait("dp", "1,1")]
-        [Fact]
-        public void Test1()
-        {
-            string name = Day01.DayName;
-            Assert.Equal("01", name);
-        }
+    [Trait("dp", "1,1")]
+    [Fact]
+    public void Test1()
+    {
+        string name = Day01.DayName;
+        Assert.Equal("01", name);
+    }
 
-        [Trait("dp", "1,2")]
-        [Fact]
-        public void Test2()
-        {
-            Console.WriteLine(AOCUtils.GetRawInput(Day01.DayName));
+    [Trait("dp", "1,2")]
+    [Fact]
+    public void Test2()
+    {
+        Console.WriteLine(AOCUtils.GetRawInput(Day01.DayName));
 
-            string name = Day01.DayName;
-            Assert.NotEqual("02", name);
-        }
+        string name = Day01.DayName;
+        Assert.NotEqual("02", name);
     }
 }
