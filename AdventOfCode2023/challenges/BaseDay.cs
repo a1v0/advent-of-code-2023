a80cb99a86681fb9d5fd5ad5236a5483d28063bd
a1v0 <avc285@gmail.com>
2023-11-22 12:10:21 +0000

    Create BaseDay class to avoid repetition on each day

---
+++ AdventOfCode2023/challenges/BaseDay.cs
@@ -0,0 +1,18 @@
+namespace AdventOfCode2023;
+
+public class BaseDay
+{
+    public string DayName
+    {
+        get
+        {
+            //
+            //
+            //
+            // logic to extract day name from class name
+            // return exception or something if no day name is found
+            // perhaps also create unit tests for this method
+            //
+        }
+    }
+}
