0c0ac40827484b09e7f00a41c27ecbcdaa03c613
a1v0 <avc285@gmail.com>
2023-11-23 11:32:08 +0000

    Move switch statement to separate method

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -4,31 +4,25 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine(Day01.DayName);
+        Console.WriteLine(AOCUtils.GetRawInput(Day01.DayName));
+
+        
         var parsedArgs = AOCUtils.ParseInitialArgs(args);
         (chosenDay, chosenTask) = parsedArgs;  
 
+       SolveChosenTask(chosenDay, chosenTask);
+        
+    }
 
-        //
-        //
-        //
-        //
-        //
-        //
-        // since this switch is likely to get pretty big, this could be moved into a separate method on AOCUtils
-        // is there a way to call a class based off a string? E.g. "Day" + chosenDay; ?
-        //   - there IS a way to do this, but it's slow. Having a switch is preferred
+   private static void SolveChosenTask(int chosenDay, int chosenTask)
+    {
         switch (chosenDay) {
             case 1:
-                Day01.Solve(chosenChallenge);
+                Day01.Solve(chosenTask);
                 break;
             default:
                 throw new Exception("Something went wrong. Please try again.");
-                return;
         }
-        
-        
-        Console.WriteLine(Day01.DayName);
-        Console.WriteLine(AOCUtils.GetRawInput(Day01.DayName));
     }
 }
-
