8dc69fef780fc0f5eaa64c96a0c5cf684aac66ae
a1v0 <avc285@gmail.com>
2023-12-01 10:28:55 +0000

    Set UnsanitisedCalibrations as private property to avoid need for params

--- AdventOfCode2023/tasks/Day01.cs
+++ AdventOfCode2023/tasks/Day01.cs
@@ -15,17 +15,21 @@ public class Day01 : BaseDay
 
 public class Day01Task1 : BaseTask
 {
+
+    private string[] UnsanitisedCalibrations
+    {
+    get;
+    } = ParseInput();
+
     public override string Solve()
     {
-        string[] unsanitisedCalibrations = ParseInput();
-        int[] calibrations = SanitiseCalibrations(unsanitisedCalibrations);
+        int[] calibrations = SanitiseCalibrations();
     }
 
-    private int[] SanitiseCalibrations(string[] unsanitisedCalibrations)
+    private int[] SanitiseCalibrations()
     {
-        int[] sanitisedCalibrations = new int[unsanitisedCalibrations.Length];
+        int[] sanitisedCalibrations = new int[UnsanitisedCalibrations.Length];
         // 
-        // set unsanitised calibrations as private property to avoid the need for params
         // loop through each line
         // loop through each char
         // since TryParse will converrt chars to ints, it's probably easier for now to use a regex to check for no. from 1–9
