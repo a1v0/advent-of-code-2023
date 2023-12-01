6ff6026fcbc9b1f96b6d3698fa92ed8757d16f39
a1v0 <avc285@gmail.com>
2023-12-01 10:06:17 +0000

    Plan day's approach

--- AdventOfCode2023/tasks/Day01.cs
+++ AdventOfCode2023/tasks/Day01.cs
@@ -18,6 +18,23 @@ public class Day01Task1 : BaseTask
     public override string Solve()
     {
         string[] unsanitisedCalibrations = ParseInput();
+        int[] calibrations = SanitiseCalibrations(unsanitisedCalibrations);
+    }
+
+    private int[] SanitiseCalibrations(string[] unsanitisedCalibrations)
+    {
+        int[] sanitisedCalibrations = new int[unsanitisedCalibrations.Length];
+        // 
+        // set unsanitised calibrations as private property to avoid the need for params
+        // loop through each line
+        // loop through each char
+        // since TryParse will converrt chars to ints, it's probably easier for now to use a regex to check for no. from 1–9
+        // try using one of C#'s double loops to count down as well as up
+        // ensure that, if there's only one int, that the int doesn't get counted twice
+        // combine both numbers, convert to int and add to sanitisedCals
+        // sum array
+        // return
+        // 
     }
 
     private string[] ParseInput()
