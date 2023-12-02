92e17fa7010df545d50b9b7581d1f3711422d510
a1v0 <avc285@gmail.com>
2023-12-02 22:12:05 +0000

    Create Day 2 class

---
+++ AdventOfCode2023/tasks/Day02.cs
@@ -0,0 +1,22 @@
+namespace AdventOfCode2023;
+
+public class Day02 : BaseDay
+{
+    public override BaseTask Task1
+    {
+        get;
+    } = new Day01Task1();
+
+    public override BaseTask Task2
+    {
+        get;
+    } = new Day01Task2();
+}
+
+public class Day02Task1 : BaseTask
+{
+}
+
+public class Day02Task2 : Day01Task1
+{
+}
