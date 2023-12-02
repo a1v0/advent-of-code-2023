7899d6b958b20deb36520a82f963457811173fbb
a1v0 <avc285@gmail.com>
2023-12-02 22:20:18 +0000

    Plan approach

--- AdventOfCode2023/tasks/Day02.cs
+++ AdventOfCode2023/tasks/Day02.cs
@@ -15,6 +15,14 @@ public class Day02 : BaseDay
 
 public class Day02Task1 : BaseTask
 {
+    public override string Solve()
+    {
+        // - parse input as rows
+        // - parse rows as games
+        // - parse games as subsets
+        // - parse subsets as dictionary or similar
+        // - loop through each game to see whether it fits the desired pattern
+    }
 }
 
 public class Day02Task2 : Day01Task1
