d752902c34d1ce792e8189182bbf007a680473d2
a1v0 <avc285@gmail.com>
2023-12-02 18:03:07 +0000

    Add initial line break to output CW

--- AdventOfCode2023/AOCUtils.cs
+++ AdventOfCode2023/AOCUtils.cs
@@ -25,7 +25,7 @@ public class AOCUtils
         string titleRow = GetResultRow("Day " + dayName, border.Length),
             resultRow = GetResultRow(result, border.Length);
 
-        string output = $"{border}\n{titleRow}\n{resultRow}\n{border}";
+        string output = $"\n{border}\n{titleRow}\n{resultRow}\n{border}";
         Console.WriteLine(output);
     }
 
