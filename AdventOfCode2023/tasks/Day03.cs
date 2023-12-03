78ee07bc93e4c37d29dfe3ef02a9d8dbc431cdf1
a1v0 <avc285@gmail.com>
2023-12-03 19:13:15 +0000

    Create blank solution file

---
+++ AdventOfCode2023/tasks/Day03.cs
@@ -0,0 +1,25 @@
+namespace AdventOfCode2023;
+
+public class Day03 : BaseDay
+{
+    public override BaseTask Task1
+    {
+        get;
+    } = new Day03Task1();
+
+    public override BaseTask Task2
+    {
+        get;
+    } = new Day03Task2();
+}
+
+public class Day03Task1 : BaseTask
+{
+    public override string Solve()
+    {
+
+    }
+}
+
+public class Day03Task2 : Day03Task1
+{ }
