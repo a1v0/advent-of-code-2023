13cc73273d173c16fb901a05b0ad98a95ed5b18f
a1v0 <avc285@gmail.com>
2023-12-03 16:05:12 +0000

    Create boilerplate for Game class

--- AdventOfCode2023/tasks/Day02.cs
+++ AdventOfCode2023/tasks/Day02.cs
@@ -5,12 +5,12 @@ public class Day02 : BaseDay
     public override BaseTask Task1
     {
         get;
-    } = new Day01Task1();
+    } = new Day02Task1();
 
     public override BaseTask Task2
     {
         get;
-    } = new Day01Task2();
+    } = new Day02Task2();
 }
 
 public class Day02Task1 : BaseTask
@@ -19,12 +19,28 @@ public class Day02Task1 : BaseTask
     {
         // - parse input as rows
         // - parse rows as games
-        // - parse games as subsets
-        // - parse subsets as dictionary or similar
+        //   - Game is a new class
+        // - parse games as array of subsets
+        // - parse subsets as dictionary or tuple
         // - loop through each game to see whether it fits the desired pattern
+    }
+
+    private string[] GetInputRows()
+    {
+        return Input.Split('\n');
     }
 }
 
 public class Day02Task2 : Day01Task1
+{ }
+public class Game
 {
+    public Game(string input)
+    {
+        // create ParseInput which returns a tuple (game name, subsets)
+    }
+
+    private readonly Dictionary<string, int>[] _subsets;
+
+    private readonly int _gameID;
 }
