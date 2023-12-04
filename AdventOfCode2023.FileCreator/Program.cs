6afc3049dab0c1375327d425df33b1f1390e74e5
a1v0 <avc285@gmail.com>
2023-12-04 22:28:45 +0000

    Move methods to separate class

--- AdventOfCode2023.FileCreator/Program.cs
+++ AdventOfCode2023.FileCreator/Program.cs
@@ -5,12 +5,32 @@
         static void Main(string[] args)
         {
             string dayName = ParseArgs(args);
-            CreateDay(dayName);
+            new FileCreatorUtils().CreateDay(dayName);
             Console.WriteLine($"Blank classes and tests created for Day {dayName}.");
             Console.WriteLine("\nDon't forget to add today to the `switch` statement in Program!");
         }
 
-        private void CreateDay(string dayName)
+        static string ParseArgs(string[] args)
+        {
+            string dayName = args[0];
+
+            bool isInt = int.TryParse(dayName, out int result);
+            var error = new Exception("Invalid argument given.");
+
+            if (!isInt) throw error;
+
+            bool resultInRange = result >= 1 && result <= 25;
+            if (!resultInRange) throw error;
+
+            if (dayName.Length == 1) dayName = "0" + dayName;
+
+            return dayName;
+        }
+    }
+
+    public class FileCreatorUtils
+    {
+        public void CreateDay(string dayName)
         {
             bool classExists = File.Exists(BasePath + $"/AdventOfCode2023/tasks/Day{dayName}.cs"),
             testExists = File.Exists(BasePath + $"/AdventOfCode2023.Tests/Day{dayName}Tests.cs");
@@ -40,23 +60,6 @@
             {
                 return "/home/alvo/advent-of-code/advent-of-code-2023";
             }
-        }
-
-        static string ParseArgs(string[] args)
-        {
-            string dayName = args[0];
-
-            bool isInt = int.TryParse(dayName, out int result);
-            var error = new Exception("Invalid argument given.");
-
-            if (!isInt) throw error;
-
-            bool resultInRange = result >= 1 && result <= 25;
-            if (!resultInRange) throw error;
-
-            if (dayName.Length == 1) dayName = "0" + dayName;
-
-            return dayName;
         }
     }
 }
