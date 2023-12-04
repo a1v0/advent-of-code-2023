ede22fc233352ee2f9ce87884f310aa0fb2f5b27
a1v0 <avc285@gmail.com>
2023-12-04 22:03:16 +0000

    Create FileCreator project

---
+++ AdventOfCode2023.FileCreator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2023.FileCreator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
