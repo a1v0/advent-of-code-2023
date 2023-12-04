15dcb58ab6d6dbf131aad64ce88bbb6dd8fd8bfc
a1v0 <avc285@gmail.com>
2023-12-04 22:09:49 +0000

    Create ParseArgs

--- AdventOfCode2023.FileCreator/Program.cs
+++ AdventOfCode2023.FileCreator/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dayName = ParseArgs(args);
+        }
+
+        static void CreateDay()
+        {
+            string basePath = "/home/alvo/advent-of-code/advent-of-code-2023";
+        }
+
+        static string ParseArgs(string[] args)
+        {
+            bool isInt = int.TryParse(args[0], out int result);
+            var error = new Exception("Invalid argument given.");
+
+            if (!isInt) throw error;
+
+            bool resultInRange = result >= 1 && result <= 25;
+            if (!resultInRange) throw error;
+
+            return args[0];
         }
     }
 }
