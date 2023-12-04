a7c1ed11d4b62a433745222ef94b6e41b4290552
a1v0 <avc285@gmail.com>
2023-12-04 22:23:52 +0000

    Complete CreateDay

--- AdventOfCode2023.FileCreator/Program.cs
+++ AdventOfCode2023.FileCreator/Program.cs
@@ -7,13 +7,40 @@
             string dayName = ParseArgs(args);
             CreateDay(dayName);
             Console.WriteLine($"Blank classes and tests created for Day {dayName}.");
+            Console.WriteLine("\nDon't forget to add today to the `switch` statement in Program!");
         }
 
-        static void CreateDay(string dayName)
+        private void CreateDay(string dayName)
         {
+            bool classExists = File.Exists(BasePath + $"/AdventOfCode2023/tasks/Day{dayName}.cs"),
+            testExists = File.Exists(BasePath + $"/AdventOfCode2023.Tests/Day{dayName}Tests.cs");
+
+            if (classExists)
+            {
+                Console.WriteLine($"Class file for Day {dayName} already exists.");
+            }
+            else
+            {
+                // otherwise create file
+            }
+
+            if (testExists)
+            {
+                Console.WriteLine($"Test file for Day {dayName} already exists.");
+            }
+            else
+            {
+                // otherwise create file
+            }
         }
 
-        public string BasePath { get; } = "/home/alvo/advent-of-code/advent-of-code-2023";
+        public string BasePath
+        {
+            get
+            {
+                return "/home/alvo/advent-of-code/advent-of-code-2023";
+            }
+        }
 
         static string ParseArgs(string[] args)
         {
