2feac758e7edd1199411a1c9d8ee707e62740b47
a1v0 <avc285@gmail.com>
2023-12-07 12:16:11 +0000

    Create test for Part 1

--- AdventOfCode2023.Tests/Day04Tests.cs
+++ AdventOfCode2023.Tests/Day04Tests.cs
@@ -6,7 +6,7 @@ public class Day04Tests : BaseTest
     [Fact]
     public void Test1()
     {
-        const string expected = "RANDOM VALUE TO ENSURE TEST FAILS UNTIL REAL VALUE IS ENTERED";
+        const string expected = "13";
         string actual = new Day04().Solve(1);
         Assert.Equal(expected, actual);
     }
