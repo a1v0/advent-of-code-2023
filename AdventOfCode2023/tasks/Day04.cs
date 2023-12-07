dd0b62c1ebe52212d6b4cdfeb7568efcecea099e
a1v0 <avc285@gmail.com>
2023-12-07 13:13:33 +0000

    Create GetCards

--- AdventOfCode2023/tasks/Day04.cs
+++ AdventOfCode2023/tasks/Day04.cs
@@ -20,12 +20,33 @@ public class Day04Task1 : BaseTask
         int[] pointsForAllCards = GetPoints();
         int totalPoints = pointsForAllCards.Sum();
         return totalPoints.ToString();
+    }
+
+    private int[] GetPoints()
+    {
+        (List<int>, List<int>)[] cards = GetCards(); // maybe make this into a property
         // split into list of winning numbers and actual numbers
         // - array of tuples?
         // loop over winning numbers per day to find amount of winning numbers
         // calculate points based on amount of winning numbers
-        // sum all points
     }
+
+    private (List<int>, List<int>)[] GetCards()
+    {
+        string[] inputRows = GetInputRows();
+        var cards = new (List<int>, List<int>)[inputRows.Length];
+
+        for (int i = 0; i < inputRows.Length; ++i)
+        {
+            string row = inputRows[i];
+            (List<int>, List<int>) parsedRow = ParseRow(row);
+            cards[i] = parsedRow;
+        }
+
+        return cards;
+    }
+
+    private (List<int>, List<int>)[] Cards { get; } = GetCards();
 }
 
 public class Day04Task2 : Day04Task1
