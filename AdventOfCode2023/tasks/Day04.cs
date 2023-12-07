c915f9c25f05a73109e7c71dded1fd3137b22039
a1v0 <avc285@gmail.com>
2023-12-07 12:45:03 +0000

    Plan approach

--- AdventOfCode2023/tasks/Day04.cs
+++ AdventOfCode2023/tasks/Day04.cs
@@ -16,7 +16,15 @@ public class Day04 : BaseDay
 public class Day04Task1 : BaseTask
 {
     public override string Solve()
-    { }
+    {
+        // split into list of winning numbers and actual numbers
+        // - array of tuples?
+        // loop over winning numbers per day to find amount of winning numbers
+        // calculate points based on amount of winning numbers
+        // sum all points
+        //
+        //
+    }
 }
 
 public class Day04Task2 : Day04Task1
