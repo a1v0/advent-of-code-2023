c662f014a5a563957858565ce50078e41469f69a
a1v0 <avc285@gmail.com>
2023-12-08 18:06:30 +0000

    Plan approach

--- AdventOfCode2023/tasks/Day05.cs
+++ AdventOfCode2023/tasks/Day05.cs
@@ -16,7 +16,20 @@ public class Day05 : BaseDay
 public class Day05Task1 : BaseTask
 {
     public override string Solve()
-    { }
+    {
+        // parse input into individual maps (could be a separate class)
+        // maps are arrays of tuples (source, translation, length)
+        // maps are sorted by source number
+        // loop over seed numbers, then:
+        // loop over map contents
+        // - if seed number >= source number, then check whether seed number is within range
+        // - if not, move on to next one
+        // - if seed number < source, or if you make it through the map without finding a match, then return seed number unmapped
+        // - if you find a match, find index of match within range, then add to translation. return match
+        // plug this value into the next map
+        // put final number into array of location numbers
+        // find smallest location number, stringify, return
+    }
 }
 
 public class Day05Task2 : Day05Task1
