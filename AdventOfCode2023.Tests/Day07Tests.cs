b3627eae0e7e242c5c890ee002b4fb17d6b6040c
a1v0 <avc285@gmail.com>
2023-12-09 17:48:53 +0000

    Create day's boilerplate

---
+++ AdventOfCode2023.Tests/Day07Tests.cs
@@ -0,0 +1,22 @@
+namespace AdventOfCode2023.Tests;
+
+public class Day07Tests : BaseTest
+{
+    [Trait("dp", "7,1")]
+    [Fact]
+    public void Test1()
+    {
+        const string expected = "RANDOM VALUE TO ENSURE TEST FAILS UNTIL REAL VALUE IS ENTERED";
+        string actual = new Day07().Solve(1);
+        Assert.Equal(expected, actual);
+    }
+
+    [Trait("dp", "7,2")]
+    [Fact]
+    public void Test2()
+    {
+        const string expected = "RANDOM VALUE TO ENSURE TEST FAILS UNTIL REAL VALUE IS ENTERED";
+        string actual = new Day07().Solve(2);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ AdventOfCode2023/tasks/Day07.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode2023;
+
+public class Day07 : BaseDay
+{
+    public override BaseTask Task1
+    {
+        get;
+    } = new Day07Task1();
+
+    public override BaseTask Task2
+    {
+        get;
+    } = new Day07Task2();
+}
+
+public class Day07Task1 : BaseTask
+{
+    public override string Solve()
+    { }
+}
+
+public class Day07Task2 : Day07Task1
+{ }
