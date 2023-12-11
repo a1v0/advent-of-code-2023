af45f22970bb61a1d67d66da6962d760ca63e342
a1v0 <avc285@gmail.com>
2023-12-11 21:42:38 +0000

    Plan approach

--- AdventOfCode2023/tasks/Day07.cs
+++ AdventOfCode2023/tasks/Day07.cs
@@ -16,7 +16,20 @@ public class Day07 : BaseDay
 public class Day07Task1 : BaseTask
 {
     public override string Solve()
-    { }
+    {
+        // parse input into array of custom Hand objects
+        // - int bid
+        // - string cards
+        // - int type (strongest type is 6, weakest is 0)
+        // create sorting method to sort the array
+        // - if a.type > b.type, a goes last
+        // - a.type < b.type, a goes first
+        // - a.type == b.type, use some sort of switch statement method to assess which is greater
+        // sort array
+        // iterate over array to find total score
+        // - maybe use an aggregator method, as a learning exercise
+        // return as string
+    }
 }
 
 public class Day07Task2 : Day07Task1
