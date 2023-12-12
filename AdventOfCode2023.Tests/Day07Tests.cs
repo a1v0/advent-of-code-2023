b049eecd10a7b5c1b2db78a6ad1cfc1065d83130
a1v0 <avc285@gmail.com>
2023-12-12 21:57:23 +0000

    Update `expected` value for Test 2

--- AdventOfCode2023.Tests/Day07Tests.cs
+++ AdventOfCode2023.Tests/Day07Tests.cs
@@ -15,7 +15,7 @@ public class Day07Tests : BaseTest
     [Fact]
     public void Test2()
     {
-        const string expected = "RANDOM VALUE TO ENSURE TEST FAILS UNTIL REAL VALUE IS ENTERED";
+        const string expected = "5905";
         string actual = new Day07().Solve(2);
         Assert.Equal(expected, actual);
     }
