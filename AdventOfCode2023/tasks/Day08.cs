7befa6cfd317092daeb19257f0e5e68ea6c8dcaf
a1v0 <avc285@gmail.com>
2023-12-15 21:06:42 +0000

    Add Day 8 boilerplate

---
+++ AdventOfCode2023/tasks/Day08.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode2023;
+
+public class Day08 : BaseDay
+{
+    public override BaseTask Task1
+    {
+        get;
+    } = new Day08Task1();
+
+    public override BaseTask Task2
+    {
+        get;
+    } = new Day08Task2();
+}
+
+public class Day08Task1 : BaseTask
+{
+    public override string Solve()
+    { }
+}
+
+public class Day08Task2 : Day08Task1
+{ }
