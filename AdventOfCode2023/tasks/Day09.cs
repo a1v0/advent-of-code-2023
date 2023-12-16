cdc1755aed6b73dc6692aea96f3e5926e588a3fb
a1v0 <avc285@gmail.com>
2023-12-16 19:27:38 +0000

    Create rough action plan

--- AdventOfCode2023/tasks/Day09.cs
+++ AdventOfCode2023/tasks/Day09.cs
@@ -16,7 +16,18 @@ public class Day09 : BaseDay
 public class Day09Task1 : BaseTask
 {
     public override string Solve()
-    { }
+    {
+        // create class Sequence
+        // - contents, array
+        // - child (optional), Sequence
+        // - next value, recursive method
+        // parse input as array of Sequences
+        // create list of ints to store next values
+        // - iterate through Sequences and find next value
+        // - add to list
+        // sum list
+        // return as string
+    }
 }
 
 public class Day09Task2 : Day09Task1
