1411a3dec56ead25ab359d799f8c4ea7d4b434df
a1v0 <avc285@gmail.com>
2023-12-27 21:36:12 +0000

    Create rough plan

--- AdventOfCode2023/tasks/Day10.cs
+++ AdventOfCode2023/tasks/Day10.cs
@@ -16,7 +16,20 @@ public class Day10 : BaseDay
 public class Day10Task1 : BaseTask
 {
     public override string Solve()
-    { }
+    {
+        // create class for pipe sections
+        // - coordinates
+        // - value
+        // parse input as a list of pipe sections
+        // find coordinates of S, then start parsing
+        // look in all four directions to find a direction to go in
+        // - switch to find next coordinate
+        // - e.g. if current value is L, look in the appropriate direction to find next one
+        // add new coordinate to end of list
+        // ensure you handle the possibility of an out-of-bounds exception in X and Y
+        // add node to list and follow the path
+        // return length of list / 2 (figure out whether to round up or down if it's an odd number)
+    }
 }
 
 public class Day10Task2 : Day10Task1
