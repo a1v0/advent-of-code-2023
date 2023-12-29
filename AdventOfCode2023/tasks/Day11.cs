2d5a6270b207a3f510cae91e3a2d528521b5cd3f
a1v0 <avc285@gmail.com>
2023-12-29 13:28:19 +0000

    Create `Galaxy` class

--- AdventOfCode2023/tasks/Day11.cs
+++ AdventOfCode2023/tasks/Day11.cs
@@ -40,3 +40,38 @@ public class Day11Task1 : BaseTask
 
 public class Day11Task2 : Day11Task1
 { }
+
+public class Galaxy
+{
+    public Galaxy(int id, int x, int y)
+    {
+        _id = id;
+        _x = x;
+        _y = y;
+    }
+
+    private int _id;
+    public int ID
+    {
+        get
+        {
+            return _id;
+        }
+    }
+    private int _x;
+    public int X
+    {
+        get
+        {
+            return _x;
+        }
+    }
+    private int _y;
+    public int Y
+    {
+        get
+        {
+            return _y;
+        }
+    }
+}
