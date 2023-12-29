bb87dbfb35d12d520ed88f08134b020f0841789f
a1v0 <avc285@gmail.com>
2023-12-29 10:43:09 +0000

    Create `expected` value for Part 1

--- AdventOfCode2023.Tests/Day11Tests.cs
+++ AdventOfCode2023.Tests/Day11Tests.cs
@@ -6,7 +6,7 @@ public class Day11Tests : BaseTest
     [Fact]
     public void Test1()
     {
-        const string expected = "RANDOM_VALUE_TO_ENSURE_TEST_FAILS_UNTIL_REAL_VALUE_IS_ENTERED";
+        const string expected = "374";
         string actual = new Day11().Solve(1);
         Assert.Equal(expected, actual);
     }
