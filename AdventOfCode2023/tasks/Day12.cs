a9e472781334466ad49eaf5274e87cc573baf2d9
a1v0 <avc285@gmail.com>
2023-12-29 20:35:28 +0000

    Create boilerplate

---
+++ AdventOfCode2023/tasks/Day12.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode2023;
+
+public class Day12 : BaseDay
+{
+    public override BaseTask Task1
+    {
+        get;
+    } = new Day12Task1();
+
+    public override BaseTask Task2
+    {
+        get;
+    } = new Day12Task2();
+}
+
+public class Day12Task1 : BaseTask
+{
+    public override string Solve()
+    { }
+}
+
+public class Day12Task2 : Day12Task1
+{ }
