196e81d683be8fe959c14272413352ecf9a577c5
a1v0 <avc285@gmail.com>
2023-12-30 11:23:36 +0000

    Plan Day 12

--- AdventOfCode2023/tasks/Day12.cs
+++ AdventOfCode2023/tasks/Day12.cs
@@ -16,7 +16,38 @@ public class Day12 : BaseDay
 public class Day12Task1 : BaseTask
 {
     public override string Solve()
-    { }
+    {
+        // create class ConditionRecord
+        // - string of content (or maybe bool? array where true=#, false=. and null=?)
+        // - array of quantities
+        // - int of total unknown damaged springs (i.e. total damaged springs minus known damaged springs)
+        // - int of amount of different combinations
+        // - regex based on quantities
+        // use quantities to create regexes
+        // - something like "^.*" + ["####", "#" etc.].Join(".+") + ".*$"
+        // test everything against this regex
+        // 
+        // calculate total number of combinations
+        // - using quantity of unknown damaged springs, add them to the string
+        //   - if quantity is 0, then set combinations to 1 and break
+        // 
+        // backtracking solution (possibly very inefficient)
+        // - if no more ? in string OR if we've run out of #s to distribute
+        //   - set all ? to . and test with regex
+        //   - if a match, ++, otherwise no
+        //   - return
+        // - otherwise change next ? with # and run recursive method again
+        // - as above but with .
+        // 
+        // backtracking method details:
+        // - copy string
+        // - find next index of ? and replace with #, then .
+        // - run method again recursively
+        // 
+        // create array of ConditionRecords
+        // sum different combinations
+        // return
+    }
 }
 
 public class Day12Task2 : Day12Task1
