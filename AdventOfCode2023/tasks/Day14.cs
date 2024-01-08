078d051306b445daf3bd83c6aed8aa9b0fcac89a
a1v0 <avc285@gmail.com>
2024-01-08 21:37:34 +0000

    Create plan for the task

--- AdventOfCode2023/tasks/Day14.cs
+++ AdventOfCode2023/tasks/Day14.cs
@@ -16,7 +16,18 @@ public class Day14 : BaseDay
 public class Day14Task1 : BaseTask
 {
     public override string Solve()
-    { }
+    {
+        // parse input into columns
+        // split columns by "#"
+        // turn each block char array and sort such that O and . are separate
+        // join by "#"
+        // 
+        // summarise each column
+        // - loop through column's contents and add
+        // sum all columns
+        // return
+        // 
+    }
 }
 
 public class Day14Task2 : Day14Task1
