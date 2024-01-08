11de7a68e378e178257330c40bfbe559f68c3239
a1v0 <avc285@gmail.com>
2024-01-08 22:15:58 +0000

    Create `Columns` property

--- AdventOfCode2023/tasks/Day14.cs
+++ AdventOfCode2023/tasks/Day14.cs
@@ -28,6 +28,36 @@ public class Day14Task1 : BaseTask
         // return
         // 
     }
+
+    private string[]? _columns;
+    private string[] Columns
+    {
+        get
+        {
+            _columns ??= GetColumns();
+            return _columns;
+        }
+    }
+
+    private string[] GetColumns()
+    {
+        int totalColumns = InputRows[0].Length;
+        string[] columns = new string[totalColumns];
+
+        for (int i = 0; i < totalColumns; ++i)
+        {
+            string column = "";
+
+            foreach (string row in InputRows)
+            {
+                column += row[i];
+            }
+
+            columns[i] = column;
+        }
+
+        return columns;
+    }
 }
 
 public class Day14Task2 : Day14Task1
