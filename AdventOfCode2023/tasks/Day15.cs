a9fe63beaaa2f2e97a5581a87df8fa8f8bc12aa1
a1v0 <avc285@gmail.com>
2024-01-23 20:16:20 +0000

    Plan approach

--- AdventOfCode2023/tasks/Day15.cs
+++ AdventOfCode2023/tasks/Day15.cs
@@ -16,7 +16,18 @@ public class Day15 : BaseDay
 public class Day15Task1 : BaseTask
 {
     public override string Solve()
-    { }
+    {
+        // parse input as array of strings
+        // loop through each string and run HASH on it
+        // - initialise current at 0
+        // - loop through each char
+        //   - (int)char casts it as its ASCII value
+        // - add ascii to current
+        // - multiply by 17
+        // - divide by 256 and return remainder
+        // 
+        // summarise and return as string
+    }
 }
 
 public class Day15Task2 : Day15Task1
