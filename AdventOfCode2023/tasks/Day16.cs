9e70b215efdf4d1706687bf92e2ca4d052b9a81c
a1v0 <avc285@gmail.com>
2024-01-24 21:16:41 +0000

    Rename `Path` to `LightBeam`

--- AdventOfCode2023/tasks/Day16.cs
+++ AdventOfCode2023/tasks/Day16.cs
@@ -17,17 +17,20 @@ public class Day16Task1 : BaseTask
 {
     public override string Solve()
     {
-        // create Path class:
+        // create LightBeam class:
         // - direction
+        // 
+        // 
+        // 
         // - current position (int x, int y)
         // 
         // parse input as dictionary:
         // - key: tuple of coordinates (x, y)
         // - value: Tile
         // 
-        // create List of paths
-        // create one blank path starting at 0,0, heading east
-        // loop over all paths (loop length will continue to grow over time)
+        // create List of LightBeams
+        // create one blank LightBeam starting at 0,0, heading east
+        // loop over all LightBeams (loop length will continue to grow over time)
         // 
         // while loop to keep going until path is exhausted:
         // if current tile's approachFromNorth property is true, break loop 
@@ -35,7 +38,7 @@ public class Day16Task1 : BaseTask
         // set current tile's energised property to true
         // if direction is east, add 1 to current x
         // if new tile is /, change direction accordingly
-        // if tile is a splitter and perpendicular to current direction, go in one direction and create a new path that goes in the other
+        // if tile is a splitter and perpendicular to current direction, go in one direction and create a new LightBeam that goes in the other
         // 
         // loop through dictionary of tiles and count the amount of energised ones
         // stringify and return
@@ -45,13 +48,14 @@ public class Day16Task1 : BaseTask
 public class Day16Task2 : Day16Task1
 { }
 
-public class Path
+public class LightBeam
 {
-    public Path(int x, int y)
+    public LightBeam(int x, int y)
     {
         X = x;
         Y = y;
     }
+
     public int X { get; set; }
     public int Y { get; set; }
 }
