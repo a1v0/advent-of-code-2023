c8c67af3f538e5a65bce095054f2c0650f79737f
a1v0 <avc285@gmail.com>
2024-01-26 20:44:04 +0000

    Create boilerplate for Day 17

---
+++ AdventOfCode2023.Tests/Day17Tests.cs
@@ -0,0 +1,22 @@
+namespace AdventOfCode2023.Tests;
+
+public class Day17Tests : BaseTest
+{
+    [Trait("dp", "17,1")]
+    [Fact]
+    public void Test1()
+    {
+        const string expected = "RANDOM_VALUE_TO_ENSURE_TEST_FAILS_UNTIL_REAL_VALUE_IS_ENTERED";
+        string actual = new Day17().Solve(1);
+        Assert.Equal(expected, actual);
+    }
+
+    [Trait("dp", "17,2")]
+    [Fact]
+    public void Test2()
+    {
+        const string expected = "RANDOM_VALUE_TO_ENSURE_TEST_FAILS_UNTIL_REAL_VALUE_IS_ENTERED";
+        string actual = new Day17().Solve(2);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ AdventOfCode2023/tasks/Day17.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode2023;
+
+public class Day17 : BaseDay
+{
+    public override BaseTask Task1
+    {
+        get;
+    } = new Day17Task1();
+
+    public override BaseTask Task2
+    {
+        get;
+    } = new Day17Task2();
+}
+
+public class Day17Task1 : BaseTask
+{
+    public override string Solve()
+    { }
+}
+
+public class Day17Task2 : Day17Task1
+{ }
