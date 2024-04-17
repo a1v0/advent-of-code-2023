72bdc7ecc7a90de9f815333b1b032bea43d82d29
a1v0 <avc285@gmail.com>
2024-04-17 21:18:17 +0100

    Create base items for Day 18

---
+++ AdventOfCode2023/tasks/Day18.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode2023;
+
+public class Day18 : BaseDay
+{
+    public override BaseTask Task1
+    {
+        get;
+    } = new Day18Task1();
+
+    public override BaseTask Task2
+    {
+        get;
+    } = new Day18Task2();
+}
+
+public class Day18Task1 : BaseTask
+{
+    public override string Solve()
+    { }
+}
+
+public class Day18Task2 : Day18Task1
+{ }
