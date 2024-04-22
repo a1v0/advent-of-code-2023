2ae879052a467edd12cd07db90547625dd13df09
a1v0 <avc285@gmail.com>
2024-04-22 21:46:53 +0100

    Add answer to Part 2 test

--- AdventOfCode2023.Tests/Day18Tests.cs
+++ AdventOfCode2023.Tests/Day18Tests.cs
@@ -15,7 +15,7 @@ public class Day18Tests : BaseTest
     [Fact]
     public void Test2()
     {
-        const string expected = "RANDOM_VALUE_TO_ENSURE_TEST_FAILS_UNTIL_REAL_VALUE_IS_ENTERED";
+        const string expected = "952408144115";
         string actual = new Day18().Solve(2);
         Assert.Equal(expected, actual);
     }
