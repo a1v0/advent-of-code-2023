d8ec5e26e00658721f489142113ef2cc80663a79
a1v0 <avc285@gmail.com>
2024-06-07 22:38:25 +0100

    Create Accept method

--- AdventOfCode2023/tasks/Day19Utils.cs
+++ AdventOfCode2023/tasks/Day19Utils.cs
@@ -2,50 +2,61 @@ namespace AdventOfCode2023;
 
 public class MachinePart
 {
-  public MachinePart(string machineData)
-  {
-    (int x,int m,int a,int s) = ParseMachineData(machineData);
-    _x = x;
-    _m = m;
-    _a = a;
-    _s = s;
-  }
+    public MachinePart(string machineData)
+    {
+        (int x, int m, int a, int s) = ParseMachineData(machineData);
+        _x = x;
+        _m = m;
+        _a = a;
+        _s = s;
+    }
 
-  private readonly int _x;
-  public int X
-  {
-    get;
-  }
+    private readonly int _x;
+    public int X
+    {
+        get;
+    }
 
-  private readonly int _m;
-  public int M
-  {
-    get;
-  }
+    private readonly int _m;
+    public int M
+    {
+        get;
+    }
 
     private readonly int _a;
-  public int A
-  {
-    get;
-  }
+    public int A
+    {
+        get;
+    }
 
     private readonly int _s;
-  public int S
-  {
-    get;
-  }
+    public int S
+    {
+        get;
+    }
 
-  public bool? Accepted
-  {
-    get;
-    set;
-  } = null;
+    private bool? _accepted;
+    public bool? Accepted
+    {
+        get;
+    }
 
-  public bool? Rejected
-  {
-    get;
-    set;
-  } = null;
+    private bool? _rejected;
+    public bool? Rejected
+    {
+        get;
+    }
+
+    public void Accept()
+    {
+        if (Accepted is not null)
+        {
+            throw new Exception("Cannot accept or reject MachinePart object if it has already been accepted or rejected.");
+        }
+
+        _accepted = true;
+        _rejected = false;
+    }
 }
 
 public class Workflow
