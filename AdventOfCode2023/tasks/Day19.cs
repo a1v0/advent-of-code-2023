996536633dfe270c8af80c060063d0e7a9649d65
a1v0 <avc285@gmail.com>
2024-06-07 22:19:37 +0100

    Create plan

--- AdventOfCode2023/tasks/Day19.cs
+++ AdventOfCode2023/tasks/Day19.cs
@@ -16,7 +16,25 @@ public class Day19 : BaseDay
 public class Day19Task1 : BaseTask
 {
     public override string Solve()
-    { }
+    {
+      // create MachinePart class
+      // - needs Accept and Reject methods which set Accepted and Rejected properties to true/false (they're null by default)
+      // parse parts as MachineParts and make array
+      // 
+      // create Workflow class
+      // - name
+      // - public GetNextInstruction method which takes a MachinePart as parameter, performs its checks and returns an instruction for what comes next
+      // parse Workflows as dictionary
+      // 
+      // loop through MachineParts until every one is accepted or rejected
+      // add totals and return
+      // 
+      // 
+      // 
+      // 
+      // 
+      // 
+    }
 }
 
 public class Day19Task2 : Day19Task1
