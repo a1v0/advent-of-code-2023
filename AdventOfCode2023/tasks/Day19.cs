21f6b76b75442461b5b3424fb1351b72bd33c714
a1v0 <avc285@gmail.com>
2024-06-07 21:48:09 +0100

    Add blank Day 19 class

---
+++ AdventOfCode2023/tasks/Day19.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode2023;
+
+public class Day19 : BaseDay
+{
+    public override BaseTask Task1
+    {
+        get;
+    } = new Day19Task1();
+
+    public override BaseTask Task2
+    {
+        get;
+    } = new Day19Task2();
+}
+
+public class Day19Task1 : BaseTask
+{
+    public override string Solve()
+    { }
+}
+
+public class Day19Task2 : Day19Task1
+{ }
