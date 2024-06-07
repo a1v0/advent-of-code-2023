11e3fac472f077751d10079a5647cec7cc5152c9
a1v0 <avc285@gmail.com>
2024-06-07 22:24:40 +0100

    Create constructor for MachinePary

--- AdventOfCode2023/tasks/Day19Utils.cs
+++ AdventOfCode2023/tasks/Day19Utils.cs
@@ -2,10 +2,17 @@ namespace AdventOfCode2023;
 
 public class MachinePart
 {
-
+  public MachinePart(string machineData)
+  {
+    (int x,int m,int a,int s) = ParseMachineData(machineData);
+    _x = x;
+    _m = m;
+    _a = a;
+    _s = s;
+  }
 }
 
 public class Workflow
 {
-  
+
 }
