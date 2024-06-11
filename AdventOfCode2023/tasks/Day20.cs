f8ff68f53b1ac5bda5aad61fbd705d76d46da9dc
a1v0 <avc285@gmail.com>
2024-06-11 22:47:44 +0100

    Define classes required

--- AdventOfCode2023/tasks/Day20.cs
+++ AdventOfCode2023/tasks/Day20.cs
@@ -16,7 +16,26 @@ public class Day20 : BaseDay
 public class Day20Task1 : BaseTask
 {
     public override string Solve()
-    { }
+    {
+        // Classes required:
+        // - Pulse
+        //   - private Type = "high" or "low"
+        //   - public IsHigh = bool
+        //   - public IsLow = bool (returns !IsHigh)
+        //   - public Destination = module
+        //
+        // - Module (parent of other module types)
+        //   (this could be done in conjunction with an interface)
+        //   - public Destinations = List of modules
+        //   - public void SendPulse (either create a blank method in an interface or create another method called "RunMiddleware" that does all the custom logic specific to a type of module)
+        //   - public void ReceivePulse (or better name)
+        //
+        // - Classes for each type of module
+        //
+        // - Queue
+        //   - basically this just needs a static List of pulses that can be added to
+        //   - the program would iterate over the contents of this list in some way
+    }
 }
 
 public class Day20Task2 : Day20Task1
