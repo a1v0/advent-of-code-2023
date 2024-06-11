d711773a5234dfef476a33094323d0d6228e928e
a1v0 <avc285@gmail.com>
2024-06-11 22:09:49 +0100

    Create blank files for Day 20

---
+++ AdventOfCode2023/tasks/Day20.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode2023;
+
+public class Day20 : BaseDay
+{
+    public override BaseTask Task1
+    {
+        get;
+    } = new Day20Task1();
+
+    public override BaseTask Task2
+    {
+        get;
+    } = new Day20Task2();
+}
+
+public class Day20Task1 : BaseTask
+{
+    public override string Solve()
+    { }
+}
+
+public class Day20Task2 : Day20Task1
+{ }
