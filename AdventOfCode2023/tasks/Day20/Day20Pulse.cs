d91418bbc67ebcf626410337d092697cecff0e3b
a1v0 <avc285@gmail.com>
2024-06-12 21:49:15 +0100

    Create Pulse constructor

--- AdventOfCode2023/tasks/Day20/Day20Pulse.cs
+++ AdventOfCode2023/tasks/Day20/Day20Pulse.cs
@@ -2,5 +2,14 @@ namespace AdventOfCode2023;
 
 public class Pulse
 {
-  
+    public Pulse(string type, BaseModule destination)
+    {
+        if (type!="high"&&type!="low")
+        {
+            throw new Exception("Invalid Pulse type given: '"+type+"'.");
+        }
+
+        _type = type;
+        _destination = destination;
+    }
 }
