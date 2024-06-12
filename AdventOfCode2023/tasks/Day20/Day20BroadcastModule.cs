f7084029603d920415a11ec28a407b9247968c7b
a1v0 <avc285@gmail.com>
2024-06-12 22:24:43 +0100

    Override IngestPulse for Broadcaster

--- AdventOfCode2023/tasks/Day20/Day20BroadcastModule.cs
+++ AdventOfCode2023/tasks/Day20/Day20BroadcastModule.cs
@@ -2,5 +2,14 @@ namespace AdventOfCode2023;
 
 public class BroadcastModule : BaseModule, IBaseModule
 {
-  
+    public void IngestPulse(Pulse pulse)
+    {
+        /**
+         * I'm not sure whether this is a best practice,
+         * but I'm returning an error if this method is
+         * called, because the broadcaster oughtn't receive
+         * any pulses.
+         **/
+      throw new Exception("A Broadcaster object cannot ingest a pulse.");
+    }
 }
--- AdventOfCode2023/tasks/Day20/Day20FlipFlopModule.cs
+++ AdventOfCode2023/tasks/Day20/Day20FlipFlopModule.cs
@@ -2,5 +2,5 @@ namespace AdventOfCode2023;
 
 public class FlipFlopModule : BaseModule, IBaseModule
 {
-  
+    
 }
