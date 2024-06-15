c4e9240dbba14303d40c21c760956fe784fd52d3
a1v0 <avc285@gmail.com>
2024-06-15 21:37:21 +0100

    Add blank methods from interface

--- AdventOfCode2023/tasks/Day20/Day20ConjunctionModule.cs
+++ AdventOfCode2023/tasks/Day20/Day20ConjunctionModule.cs
@@ -2,5 +2,13 @@ namespace AdventOfCode2023;
 
 public class ConjunctionModule : BaseModule, IBaseModule
 {
-
+    public void EmitPulses()
+    {
+        
+    }
+    
+    public void IngestPulse(Pulse pulse)
+    {
+        
+    }
 }
--- AdventOfCode2023/tasks/Day20/Day20FlipFlopModule.cs
+++ AdventOfCode2023/tasks/Day20/Day20FlipFlopModule.cs
@@ -2,5 +2,13 @@ namespace AdventOfCode2023;
 
 public class FlipFlopModule : BaseModule, IBaseModule
 {
+    public void EmitPulses()
+    {
+        
+    }
 
+    public void IngestPulse(Pulse pulse)
+    {
+        
+    }
 }
