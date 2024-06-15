031791f2a6e566b424b1f2f236334eb4b8053cec
a1v0 <avc285@gmail.com>
2024-06-15 21:41:45 +0100

    Create IsOn property

--- AdventOfCode2023/tasks/Day20/Day20FlipFlopModule.cs
+++ AdventOfCode2023/tasks/Day20/Day20FlipFlopModule.cs
@@ -2,9 +2,28 @@ namespace AdventOfCode2023;
 
 public class FlipFlopModule : BaseModule, IBaseModule
 {
+    private bool _isOn = false;
+    private bool IsOn
+    {
+        get
+        {
+            return _isOn;
+        }
+
+        set
+        {
+          // not actually sure whether this is the correct way of coding this type of thing
+              _isOn = !_isOn;
+        }
+    }
+
     public void EmitPulses()
     {
-        
+        // Flip-flop modules (prefix %) are either on or off; they are initially off.
+        // If a flip-flop module receives a high pulse, it is ignored and nothing happens.
+        // However, if a flip-flop module receives a low pulse, it flips between on and off.
+        // If it was off, it turns on and sends a high pulse.
+        // If it was on, it turns off and sends a low pulse.
     }
 
     public void IngestPulse(Pulse pulse)
