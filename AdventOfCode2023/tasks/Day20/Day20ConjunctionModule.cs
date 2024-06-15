2a7bca214858b6dee1a0880509fcf5a0304fefe2
a1v0 <avc285@gmail.com>
2024-06-15 21:54:05 +0100

    Create InputModules property

--- AdventOfCode2023/tasks/Day20/Day20ConjunctionModule.cs
+++ AdventOfCode2023/tasks/Day20/Day20ConjunctionModule.cs
@@ -4,11 +4,25 @@ public class ConjunctionModule : BaseModule, IBaseModule
 {
     public void EmitPulses()
     {
-        
+        // Conjunction modules (prefix &) remember the type of the most recent
+        // pulse received from each of their connected input modules; they
+        // initially default to remembering a low pulse for each input. When
+        // a pulse is received, the conjunction module first updates its memory
+        // for that input. Then, if it remembers high pulses for all inputs, it
+        // sends a low pulse; otherwise, it sends a high pulse.
     }
 
     public void IngestPulse(Pulse pulse)
     {
         
+    }
+    
+    private Dictionary<BaseModule, string> _inputModules = new();
+    private Dictionary<BaseModule, string> InputModules
+    {
+        get
+        {
+            return _inputModules;
+        }
     }
 }
