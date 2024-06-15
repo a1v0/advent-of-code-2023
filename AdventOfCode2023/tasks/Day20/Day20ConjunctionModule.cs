35a7515f62ad1286c97e3f0f51bf0c10540e877b
a1v0 <avc285@gmail.com>
2024-06-15 22:13:14 +0100

    Delete comment

--- AdventOfCode2023/tasks/Day20/Day20ConjunctionModule.cs
+++ AdventOfCode2023/tasks/Day20/Day20ConjunctionModule.cs
@@ -4,12 +4,6 @@ public class ConjunctionModule : BaseModule, IBaseModule
 {
     public void EmitPulses()
     {
-        // Conjunction modules (prefix &) remember the type of the most recent
-        // pulse received from each of their connected input modules; they
-        // initially default to remembering a low pulse for each input. When
-        // a pulse is received, the conjunction module first updates its memory
-        // for that input. Then, if it remembers high pulses for all inputs, it
-        // sends a low pulse; otherwise, it sends a high pulse.
         string pulseType = GetPulseType();
 
         foreach (string destination in Destinations)
