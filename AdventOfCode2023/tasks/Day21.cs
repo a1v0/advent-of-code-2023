332f973368c0523100952a52adde206becca0268
a1v0 <avc285@gmail.com>
2024-07-02 22:10:33 +0100

    Create blank Day 21 class`

---
+++ AdventOfCode2023/tasks/Day21.cs
@@ -0,0 +1,23 @@
+namespace AdventOfCode2023;
+
+public class Day21 : BaseDay
+{
+    public override BaseTask Task1
+    {
+        get;
+    } = new Day21Task1();
+
+    public override BaseTask Task2
+    {
+        get;
+    } = new Day21Task2();
+}
+
+public class Day21Task1 : BaseTask
+{
+    public override string Solve()
+    { }
+}
+
+public class Day21Task2 : Day21Task1
+{ }
