af3ea511b0b70962b2a3a59d47d5b306892aa180
a1v0 <avc285@gmail.com>
2024-07-02 22:31:22 +0100

    Plan approach

--- AdventOfCode2023/tasks/Day21.cs
+++ AdventOfCode2023/tasks/Day21.cs
@@ -16,7 +16,15 @@ public class Day21 : BaseDay
 public class Day21Task1 : BaseTask
 {
     public override string Solve()
-    { }
+    {
+        // create GardenPlot class
+        // - list of neighbours
+        // - coordinates?
+        // parse input as GardenPlots
+        // create global list of "active" plots (use a datatype that can contain only unique values, similar to Set in JavaScript)
+        // loop through active plots, add each plot's neighbours to new list of active plots and deactivate current plot
+        // do this X times and return total active plots
+    }
 }
 
 public class Day21Task2 : Day21Task1
