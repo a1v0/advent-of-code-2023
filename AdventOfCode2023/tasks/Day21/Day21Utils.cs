7ff5a80e26b836519265df62608acc4ea7d61b27
a1v0 <avc285@gmail.com>
2024-07-03 18:38:44 +0100

    Remove constructor ufn

--- AdventOfCode2023/tasks/Day21/Day21Utils.cs
+++ AdventOfCode2023/tasks/Day21/Day21Utils.cs
@@ -2,11 +2,6 @@ namespace AdventOfCode2023;
 
 public class GardenPlot
 {
-    public GardenPlot((int x, int y) coordinates)
-    {
-
-    }
-
     private List<GardenPlot> _neighbours = new();
     public List<GardenPlot> Neighbours
     {
